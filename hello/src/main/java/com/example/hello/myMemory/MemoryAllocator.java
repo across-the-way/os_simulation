51ae6f85c3d8909cb15c0d97b3bba499207d53b7
yanxuebin <1296894104@qq.com>
2024-03-26 15:47:16 +0800

    完善了firstFit，补充了其余三种连续分配策略，优化release

--- hello/src/main/java/com/example/hello/myMemory/MemoryAllocator.java
+++ hello/src/main/java/com/example/hello/myMemory/MemoryAllocator.java
@@ -27,6 +27,7 @@ class ContiguousAllocator extends MemoryAllocator {
     protected Map<Integer, Block> used_memory; // 进程-内存表
     enum AllocationPolicy { FIRST_FIT, NEXT_FIT, BEST_FIT, WORST_FIT }
     private AllocationPolicy allocation_policy = AllocationPolicy.FIRST_FIT;
+    private int last_index = 0;//用于NEXT_FIT,记录上一次在free_blocks中查找结束的位置
 
     public ContiguousAllocator(int total_memory_size) {
         super(total_memory_size);
@@ -39,6 +40,9 @@ class ContiguousAllocator extends MemoryAllocator {
     Block findFreeSpace(int size) {
         switch (allocation_policy) {
             case FIRST_FIT: return firstFit(size);
+            case NEXT_FIT: return nextFit(size);
+            case BEST_FIT: return bestFit(size);
+            case WORST_FIT: return worstFit(size);
             default: break;
         }
         return null;
@@ -51,7 +55,7 @@ class ContiguousAllocator extends MemoryAllocator {
 
     @Override
     void release(int pid) {
-        Block block = used_memory.get(pid);
+        /*Block block = used_memory.get(pid);
         // 这个实现不好
         for (int i = 0; i < free_blocks.size(); i++) {
             Block free_block = free_blocks.get(i);
@@ -79,7 +83,33 @@ class ContiguousAllocator extends MemoryAllocator {
                 break;
             }
         }
+        used_memory.remove(pid);*/
+        List<Block> new_free_blocks = new ArrayList<>();
+        Block block = used_memory.get(pid);
+        int start = block.getStart();
+        int end = block.getEnd();
+        boolean placed = false;
+
+        for (int i = 0; i < free_blocks.size(); i++) {
+            Block free_block = free_blocks.get(i);
+            if (free_block.getStart() > end + 1) {
+                if (!placed) {
+                    new_free_blocks.add(new Block(start, end - start + 1));
+                    placed = true;
+                }
+                new_free_blocks.add(free_block);
+            } else if (free_block.getEnd() < start - 1) {
+                new_free_blocks.add(free_block);
+            } else {
+                start = Math.min(start, free_block.getStart());
+                end = Math.max(end, free_block.getEnd());
+            }
+        }
+        if (!placed) {
+            new_free_blocks.add(new Block(start, end - start + 1));
+        }
         used_memory.remove(pid);
+        free_blocks = new_free_blocks;
     }
 
     private Block firstFit(int size) {
@@ -88,24 +118,94 @@ class ContiguousAllocator extends MemoryAllocator {
             Block block = free_blocks.get(i);
             if (block.getSize() >= size) {
                 free_block = new Block(block.getStart(), size);
-                free_blocks.get(i).reset(block.getStart() + size, block.getSize() - size);
+                if (block.getSize() == size) { // 恰好被分配完则删除
+                    free_blocks.remove(i);
+                } else {
+                    free_blocks.get(i).reset(block.getStart() + size, block.getSize() - size);
+                }
+                last_index = i; //用于配合NEXT_FIT
                 break;
             }
         }
         return free_block;
     }
     
-    // private Block nextFit(int size) {
-        
-    // }
+    private Block nextFit(int size) {
+        Block free_block = null;
+        while (last_index >= free_blocks.size()) { //防止last_index越界
+            last_index--;
+        }
+        for (int i = last_index; ; i = (i + 1) % free_blocks.size()) {
+            Block block = free_blocks.get(i);
+            if (block.getSize() >= size) {
+                free_block = new Block(block.getStart(), size);
+                if (block.getSize() == size) { // 恰好被分配完则删除
+                    free_blocks.remove(i);
+                } else {
+                    free_blocks.get(i).reset(block.getStart() + size, block.getSize() - size);
+                }
+                last_index = i; //用于配合NEXT_FIT
+                break;
+            }
+            if (i == (last_index - 1) % free_blocks.size()) {
+                break; //已经遍历全部free_block则退出
+            }
+        }
+        return free_block;
+    }
     
-    // private Block bestFit(int size) {
-        
-    // }
+    private Block bestFit(int size) {
+        Block free_block = null;
+        Block best_block = null;
+        int best_index = 0;
+        int best_size_diff = total_memory_size + 1;
+        for (int i = 0; i < free_blocks.size(); i++) {
+            Block block = free_blocks.get(i);
+            int size_diff = block.getSize() - size;
+            if (size_diff >= 0) {
+                if (size_diff < best_size_diff) {
+                    best_index = i;
+                    best_block = block;
+                    best_size_diff = size_diff;
+                }
+            }
+        }
+        if (best_block != null) {
+            free_block = new Block(best_block.getStart(), size);
+            if (best_block.getSize() == size) { //恰好被分配完则删除
+                free_blocks.remove(best_index);
+            } else {
+                best_block.reset(best_block.getStart() + size, best_block.getSize() - size);
+            }
+            last_index = best_index;
+        }
+        return free_block;
+    }
     
-    // private Block worstFit(int size) {
-        
-    // }
+    private Block worstFit(int size) {
+        Block free_block = null;
+        Block worst_block = null;
+        int worst_index = 0;
+        int max_size = -1;
+        for (int i = 0; i < free_blocks.size(); i++) {
+            Block block = free_blocks.get(i);
+            if (block.getSize() > max_size) {
+                worst_index = i;
+                worst_block = block;
+                max_size = worst_block.getSize();
+            }
+        }
+        if (worst_block != null) {
+            free_block = new Block(worst_block.getStart(), size);
+            if (worst_block.getSize() == size) { //恰好分配完则删除
+                free_blocks.remove(worst_index);
+            } else {
+                worst_block.reset(worst_block.getStart() + size, worst_block.getSize() - size);
+            }
+            last_index = worst_index;
+        }
+        return free_block;
+    }
     private void show() {
         System.out.println("Free Blocks:");
         for (Block block : free_blocks) {
