3f0d31673c79f437fa1fc398ffce1f80c2442892
Area357 <1571786243@qq.com>
2024-04-13 13:27:23 +0800

    实现了使用LRU方法进行按需调页的内存分配器

--- hello/src/main/java/com/example/hello/myMemory/Memory.java
+++ hello/src/main/java/com/example/hello/myMemory/Memory.java
@@ -1,9 +1,14 @@
 package com.example.hello.myMemory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 public abstract class Memory {
     protected int size;
@@ -64,6 +69,63 @@ class Pages extends Memory {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Pages: ").append(pages);
+        return sb.toString();
+    }
+}
+
+class DemandPages extends Memory {
+    private Map<Integer, Pair<Boolean, Integer>> demandPages;
+    private int page_count_virtual;
+    public DemandPages(int size) {
+        super(size);
+        this.demandPages = new HashMap<>();
+        this.page_count_virtual = 0;
+    }
+    public void addDemandPage(int page_num_virtual) {
+        demandPages.put(page_num_virtual, Pair.of(false, 0));
+        page_count_virtual++;
+    }
+    public void addDemandPage(int page_num_virtual, int page_num_physical) {
+        demandPages.put(page_num_virtual, Pair.of(true, page_num_physical));
+    }
+    public void swapPageIn(int page_num_virtual, int page_num_physical) {
+        if (!demandPages.get(page_num_virtual).getLeft()) {
+            addDemandPage(page_num_virtual, page_num_physical);
+            page_count_virtual--;
+        }
+    }
+    public void swapPageOut(int page_num_virtual) {
+        if (demandPages.get(page_num_virtual).getLeft()) {
+            demandPages.put(page_num_virtual, Pair.of(false, 0));
+            page_count_virtual++;
+        }
+    }
+    public List<Pair<Integer, Integer>> getAllPhysicalPages() {
+        List<Pair<Integer, Integer>> physicalPages = new ArrayList<>();
+        for (Entry<Integer, Pair<Boolean, Integer>> entry : demandPages.entrySet()) {
+            if (entry.getValue().getLeft()) {
+                physicalPages.add(Pair.of(entry.getKey(), entry.getValue().getRight()));
+            }
+        }
+        return physicalPages;
+    }
+    public int getVirtualPageCount() {
+        return page_count_virtual;
+    }
+    public Pair<Boolean, Integer> getDemandPage(int page_num_virtual) {
+        return demandPages.get(page_num_virtual);
+    }
+    public int getPhysicalPage(int page_count_virtual) {
+        Pair<Boolean, Integer> pair = demandPages.get(page_count_virtual);
+        if (pair.getLeft()) {
+            return pair.getRight();
+        }
+        return -1; // 表示没有对应的物理页
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Demand Pages: ").append(demandPages);
         return sb.toString();
     }
 }
