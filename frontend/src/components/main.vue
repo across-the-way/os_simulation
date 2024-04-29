72be56cfee663d6e2e59e2e01dbdf3852eb36696
can <1427401632@qq.com>
2024-04-29 23:37:26 +0800

    暂存

--- frontend/src/components/main.vue
+++ frontend/src/components/main.vue
@@ -9,19 +9,53 @@ export default {
   data() {
     return {
       responseData: null,
+      form: {
+        LongTerm_CeilThreshold: 0,
+        LongTerm_FloorThreshold: '',
+        MidTermScale: '',
+        MidTerm_CeilThreshold: '',
+        MidTerm_FloorThreshold: false,
+        Memory_Size: [],
+        Page_Size: '',
+        Printer_Number: '',
+        Printer_Number: '',
+        Test_Max_Line: '',
+        Test_Max_Time: '',
+        availableMap: [
+          {string: '', value:0},
+        ],
+        CPUstrategy: null,
+        MMstrategy: null,
+      }
     }
   },
   created() {
-    axios.get(serverURL + '/', {})
-      .then(response => {
-        // 处理响应结果
-        console.log(response.data);
-        this.responseData = response.data
+    // axios.get(serverURL + '/', {})
+    //   .then(response => {
+    //     // 处理响应结果
+    //     console.log(response.data);
+    //     this.responseData = response.data
+    //   })
+    //   .catch(error => {
+    //     // 处理错误
+    //     console.error(error);
+    //   });
+  },
+  methods:{
+    handSubmit(){
+      axios.post(serverURL + '/',form)
+      .then(res => {
+        console.log(res.data);
+
       })
       .catch(error => {
-        // 处理错误
-        console.error(error);
+        console.log(error);
       });
+      localStorage.setItem('config',JSON.stringify(this.form))
+    },
+    ondelete(){
+      localStorage.clear()
+    }
   }
 }
 
@@ -30,7 +64,61 @@ export default {
 </script>
 
 <template>
-   <div>欢迎登录系统</div>
+  <el-form :model="form" label-width="auto" style="max-width: 600px">
+    <el-form-item label="Activity name">
+      <el-input v-model="form.SystemPulse" />
+    </el-form-item>
+    <el-form-item label="Activity zone">
+      <el-select v-model="form.InstructionLength" placeholder="please select your zone">
+        <el-option label="Zone one" value="shanghai" />
+        <el-option label="Zone two" value="beijing" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="Activity time">
+      <el-col :span="11">
+        <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%" />
+      </el-col>
+      <el-col :span="2" class="text-center">
+        <span class="text-gray-500">-</span>
+      </el-col>
+      <el-col :span="11">
+        <el-time-picker v-model="form.date2" placeholder="Pick a time" style="width: 100%" />
+      </el-col>
+    </el-form-item>
+    <el-form-item label="Instant delivery">
+      <el-switch v-model="form.delivery" />
+    </el-form-item>
+    <el-form-item label="Activity type">
+      <el-checkbox-group v-model="form.type">
+        <el-checkbox value="Online activities" name="type">
+          Online activities
+        </el-checkbox>
+        <el-checkbox value="Promotion activities" name="type">
+          Promotion activities
+        </el-checkbox>
+        <el-checkbox value="Offline activities" name="type">
+          Offline activities
+        </el-checkbox>
+        <el-checkbox value="Simple brand exposure" name="type">
+          Simple brand exposure
+        </el-checkbox>
+      </el-checkbox-group>
+    </el-form-item>
+    <el-form-item label="Resources">
+      <el-radio-group v-model="form.resource">
+        <el-radio value="Sponsor">Sponsor</el-radio>
+        <el-radio value="Venue">Venue</el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="Activity form">
+      <el-input v-model="form.desc" type="textarea" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">Create</el-button>
+      <el-button>Cancel</el-button>
+    </el-form-item>
+  </el-form>
+  <el-button type="primary" @click="ondelete">Create</el-button>
 </template>
 
 <style scoped>
--- frontend/src/components/processQueue.vue
+++ frontend/src/components/processQueue.vue
@@ -1,17 +1,10 @@
-<!--
 <template>
-  <el-table-v2 :columns="columns" :data="rowData" :width="700" :height="400" fixed />
+  <el-table-v2 :columns="columns" :data="rowData" :width="700" :height="600" fixed />
 </template>
 
 <script setup>
 import { serverURL } from '@/configjs/ServerURL';
 import axios from 'axios';
-
-// 定义映射表
-
-//const processQueue = getData();
-
-
 </script>
 <script>
 export default {
@@ -30,148 +23,60 @@ export default {
         { 'name2': [1, 2] },
         { 'name3': [3, 4, 5] },
       ],
-      columns: [1,1,2,3],
+      processQueues1: [],
+      columns: [1, 1, 2, 3],
       res: {
-        Waiting_Queues: [[1],[1],[1],[1],[1],[1]],
-        Second_Queue: [1],
-        Swapped_Ready_Queue: [2],
-        Swapped_Waiting_Queue:[3],
-        Ready_Queue: [5],
-        },
-      rowData: [],
-    }
-  },
-  created() {
-    this.getData()
-    let max = this.maxLength(this.processQueues) + 1
-    this.columns = this.generateColumns(max,{ width: 100 }, 'Queue-')
-    this.generateQueueData(this.processQueues, this.columns);
-  },
+        Ready_Queue: [5, 1],
+        Second_Queue: [1, 3, 4],
+        Swapped_Ready_Queue: [2, 7, 5],
+        Swapped_Waiting_Queue: [3],
 
-
-  methods: {
-    maxLength(processQueue) {
-      processQueue.reduce((max, item) => {
-        const arr = Object.values(item)[0];
-        return Math.max(max, arr.length);
-      }, 0)
-    },
-
-    generateQueueData(processQueue, columns, prefix = 'Queue-') {
-      processQueue.map((item, rowIndex) => {
-        const name = Object.keys(item)[0];
-        const queue = item[name];
-        const rowData = columns.reduce((rowData, column, columnIndex) => {
-          if (columnIndex === 0) {
-            rowData[column.dataKey] = name;
-          } else {
-            rowData[column.dataKey] = queue[columnIndex - 1] !== undefined ? ` ${queue[columnIndex - 1]}` : '';
-          }
-          return rowData;
-        }, { id: `${prefix}${rowIndex}`, parentId: null });
-
-        this.rowData = rowData
-      })
-
-    },
-    getData() {
-      axios.get(serverURL + '/process/queue')
-        .then(res => {
-          let temp = []
-          this.res = res.data
-          let wait = this.res.Waiting_Queues
-          let ready = this.res.Ready_Queue
-          let sq = this.res.Second_Queue
-          let srq = this.res.Swapped_Ready_Queue
-
-          // 使用映射表转换索引为设备名
-          wait.forEach(wa => {
-            let name = deviceMapping[wa]; // 使用映射表查找对应的设备名称
-            temp.push({ name: wa });
-          });
-
-          temp.push({ 'Ready_Queue': ready });
-          temp.push({ 'Second_Queue': sq });
-          temp.push({ 'Swapped_Ready_Queue': srq });
-          temp.push({ 'Swapped_Waiting_Queue': swq });
-          this.processQueues = temp
-
-        });
-    },
-    generateColumns(length, props, prefix = 'columns-') {
-      Array.from({ length }).map((_, columnIndex) => ({
-        ...props,
-        // key: `${prefix}${columnIndex}`,
-        dataKey: `${prefix}${columnIndex}`,
-        title: columnIndex === 0 ? 'Name' : `Column ${columnIndex - 1}`,
-        width: columnIndex === 0 ? 200 : 150,
-      }))
-    },
-
-  }
-}
-</script>
--->
-<template>
-  <el-table-v2 :columns="columns" :data="rowData" :width="700" :height="400" fixed />
-</template>
-
-<script setup>
-import { serverURL } from '@/configjs/ServerURL';
-import axios from 'axios';
-
-// 定义映射表
-
-//const processQueue = getData();
-
-
-</script>
-<script>
-export default {
-  data() {
-    return {
-      deviceMapping: {
-        0: 'Printer',
-        1: 'Keyboard',
-        2: 'FileRead',
-        3: 'FileWrite',
-        4: 'ForkWait',
-        5: 'Semaphore'
+        Waiting_Queues: [[1], [], [], [], [], [], []],
       },
-      processQueues: [
-        { 'name1': [1, 2] },
-        { 'name2': [1, 2] },
-        { 'name3': [3, 4, 5] },
-      ],
-      columns: [1,1,2,3],
-      res: {
-        Waiting_Queues: [[1,2,3],[1],[1,3],[1,4,3],[1],[1,8]],
-        Second_Queue: [1,3,4],
-        Swapped_Ready_Queue: [2,7,5],
-        Swapped_Waiting_Queue:[3,5],
-        Ready_Queue: [5,1],
-        },
       rowData: [],
     }
   },
   created() {
     // 不需要调用 this.getData() 因为 res 数据已经提供
-    this.processQueues = this.transformData(this.res);
-    let max = this.maxLength(this.processQueues) + 1;
-    this.columns = this.generateColumns(max, { width: 100 }, 'Queue-');
-    this.generateQueueData(this.processQueues);
-  },
+    axios.get(serverURL + '/process/queue')
+      .then(res => {
+        this.res = []
+        this.res = res.data
+        console.log(this.res)
+        this.transformData()
+        console.log(this.processQueues)
+        let max = this.maxLength(this.processQueues) + 2;
+        // console.log(max)
+        this.columns = this.generateColumns(max, { width: 100 }, 'Queue-');
+        this.generateQueueData(this.processQueues);
 
+      })
+    // this.getData()
+    // this.transformData()
+    // let max = this.maxLength(this.processQueues1) + 2;
+    // console.log(max)
+    // this.columns = this.generateColumns(max, { width: 100 }, 'Queue-');
+    // this.generateQueueData(this.processQueues1);
+  },
+  // updated() {
+  //   let max = this.maxLength(this.processQueues1) + 2;
+  //   console.log(max)
+  //   this.columns = this.generateColumns(max, { width: 100 }, 'Queue-');
+  //   this.generateQueueData(this.processQueues1);
+  // },
 
   methods: {
     maxLength(processQueue) {
+      console.log('a')
       return processQueue.reduce((max, item) => {
         const arr = Object.values(item)[0];
+
         return Math.max(max, arr.length);
       }, 0);
     },
 
-     generateQueueData(processQueue) {
+    generateQueueData(processQueue) {
+      console.log(processQueue)
       this.rowData = processQueue.map((item, index) => {
         const key = Object.keys(item)[0];
         const value = item[key];
@@ -182,28 +87,48 @@ export default {
         return row;
       });
     },
-    transformData(data) {
+    transformData() {
       let temp = [];
-      
+      let data = this.res
+      console.log('b')
       // 添加设备名称行
-      data.Waiting_Queues.forEach((queue, index) => {
+      this.res.Waiting_Queues.forEach((queue, index) => {
         let deviceName = this.deviceMapping[index];
         temp.push({ [deviceName]: queue });
       });
-      
+
       // 添加其他队列名称行
       temp.push({ 'Ready_Queue': data.Ready_Queue });
       temp.push({ 'Second_Queue': data.Second_Queue });
       temp.push({ 'Swapped_Ready_Queue': data.Swapped_Ready_Queue });
       temp.push({ 'Swapped_Waiting_Queue': data.Swapped_Waiting_Queue });
-
-      return temp;
+      this.processQueues = temp
     },
     getData() {
+      console.log('this.processQueues')
       axios.get(serverURL + '/process/queue')
         .then(res => {
           this.res = res.data
           console.log(res.data)
+          let temp = []
+          let data = res.data
+          // 添加设备名称行
+          this.res.Waiting_Queues.forEach((queue, index) => {
+            let deviceName = this.deviceMapping[index];
+            temp.push({ [deviceName]: queue });
+          });
+
+          // 添加其他队列名称行
+          temp.push({ 'Ready_Queue': data.Ready_Queue });
+          temp.push({ 'Second_Queue': data.Second_Queue });
+          temp.push({ 'Swapped_Ready_Queue': data.Swapped_Ready_Queue });
+          temp.push({ 'Swapped_Waiting_Queue': data.Swapped_Waiting_Queue });
+          console.log(temp)
+          this.processQueues1 = temp
+          this.processQueues = temp
+          console.log(this.processQueues, this.processQueues1, 'aa')
+          
+
         });
     },
     generateColumns(length, props, prefix = 'Column-') {
